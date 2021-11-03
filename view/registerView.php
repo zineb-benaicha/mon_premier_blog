4a6219542993aa569ebe3c8ccc5bd3778b400625
zineb benaicha <zineb.mezlef@gmail.com>
2021-11-03 14:47:28 +0100

    fixe files links in login/register views

--- view/registerView.php
+++ view/registerView.php
@@ -6,8 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Register - SB Admin</title>
-        <link href="css/styles.css" rel="stylesheet" />
+        <title>Enregistrement</title>
+        <link href="../public/css/stylesAdmin.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="bg-primary">
@@ -83,6 +83,6 @@
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
+        <script src="../public/js/scriptsAdmin.js"></script>
     </body>
 </html>
