57bc4016fa05f7af12572232a06b53101c4b2a6f
zineb benaicha <zineb.mezlef@gmail.com>
2021-11-03 14:39:16 +0100

    #10-add register and login views in project

--- view/homeView.php
+++ view/homeView.php
@@ -54,10 +54,11 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 text-center">
-                        <h2 class="text-white mt-0">We've got what you need!</h2>
+                        <h2 class="text-white mt-0">Devenez membre</h2>
                         <hr class="divider divider-light" />
-                        <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
-                        <a class="btn btn-light btn-xl" href="#services">Get Started!</a>
+                        <p class="text-white-75 mb-4">Rejoignez notre communauté et devenez membre en s'enregistrant, vous pourriez commenter les blogs et peut-être même les gérer!</p>
+                        <a class="btn btn-light btn-xl" href="view/registerView.php">S'enregistrer</a>
+                        <a class="btn btn-light btn-xl" href="view/loginView.php">Se connecter</a>
                     </div>
                 </div>
             </div>
