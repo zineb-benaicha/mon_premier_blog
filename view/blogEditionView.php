6409b0b3d0512885991d832f8f3f82dbb05f897e
zineb benaicha <zineb.mezlef@gmail.com>
2021-11-30 04:51:13 +0100

    boolen comparaison in forgotten password file

--- view/blogEditionView.php
+++ view/blogEditionView.php
@@ -25,72 +25,70 @@ if (session_id() == '') {
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header"><h3 class="text-center font-weight-light my-4">Modifier un blog post</h3></div>
                                     <div class="card-body">
-                                        
+
                                         <!-- Old blog informations-->
-                                        <?php  
-                                        if (isset($blogInformations) && !$blogInformations) {
-                                            $oldTitle = '';
-                                            $oldChapo = '';
-                                            $oldAuthor = '';
-                                            $oldContent = '';
-                                            $blogUpdateSuccess = false;
-                                        }
-                                        elseif (isset($blogInformations) && $blogInformations)  {
-                                            
-                                            $oldTitle = $blogInformations['title'];
-                                            $oldChapo = $blogInformations['chapo'];
-                                            $oldAuthor = $blogInformations['author'];
-                                            $oldContent = $blogInformations['content'];
-                                            $idBlog = $blogInformations['id'];
-                                        }
-                                        elseif (!isset($blogInformations)) {
-                                            $oldTitle = $_POST['title'];
-                                            $oldChapo = $_POST['chapo'];
-                                            $oldAuthor = $_POST['author'];
-                                            $oldContent = $_POST['content'];
-                                            $idBlog = $_GET['id_blog'];
-                                        }
-                                        ?>
-                                        <?php if (isset($blogUpdateSuccess) && !$blogUpdateSuccess):?> 
+                                        <?php
+if (isset($blogInformations) && !$blogInformations) {
+    $oldTitle = '';
+    $oldChapo = '';
+    $oldAuthor = '';
+    $oldContent = '';
+    $blogUpdateSuccess = false;
+} elseif (isset($blogInformations) && $blogInformations) {
+
+    $oldTitle = $blogInformations['title'];
+    $oldChapo = $blogInformations['chapo'];
+    $oldAuthor = $blogInformations['author'];
+    $oldContent = $blogInformations['content'];
+    $idBlog = $blogInformations['id'];
+} elseif (!isset($blogInformations)) {
+    $oldTitle = $_POST['title'];
+    $oldChapo = $_POST['chapo'];
+    $oldAuthor = $_POST['author'];
+    $oldContent = $_POST['content'];
+    $idBlog = $_GET['id_blog'];
+}
+?>
+                                        <?php if (isset($blogUpdateSuccess) && !$blogUpdateSuccess): ?>
                                             <div class="alert alert-danger" role="alert">
                                                 Une erreur est survenue veuillez réessayer plus tard.
                                             </div>
                                             <?php header("refresh:2;url=../index.php?action=displayView&viewName=adminDashboard");?>
 
-                                        <? endif ?>
+                                        <?endif?>
 
-                                        <form action="../index.php?action=updateBlog&id_blog=<?= $idBlog ?>" method="post" >
-                                            <?php if (isset($emptyFields) && $emptyFields):?> 
-                                                
+                                        <form action="../index.php?action=updateBlog&id_blog=<?=$idBlog?>" method="post" >
+                                            <?php if (isset($emptyFields) && $emptyFields): ?>
+
                                                 <div class="alert alert-danger" role="alert">
                                                     Vous devez devez remplir touts les champs !
                                                 </div>
-                                            <? endif ?>
+                                            <?endif?>
                                             <!-- Title input-->
-                                            
+
                                             <div class="form-floating mb-3 ">
-                                                <input class="form-control" id="title" type="text" name="title" value="<?= $oldTitle ?>" />
+                                                <input class="form-control" id="title" type="text" name="title" value="<?=$oldTitle?>" />
                                                 <label for="name">Titre</label>
                                             </div>
 
                                             <!-- Chapo input-->
-                                            
+
                                             <div class="form-floating mb-3 ">
-                                                <input class="form-control" id="chapo" type="text" name="chapo" value="<?= $oldChapo ?>"/>
+                                                <input class="form-control" id="chapo" type="text" name="chapo" value="<?=$oldChapo?>"/>
                                                 <label for="name">Châpo</label>
                                             </div>
 
                                             <!-- author input-->
-                                            
+
                                             <div class="form-floating mb-3 ">
-                                                <input class="form-control" id="author" type="text" name="author" value="<?= $oldAuthor ?>"/>
+                                                <input class="form-control" id="author" type="text" name="author" value="<?=$oldAuthor?>"/>
                                                 <label for="name">Auteur</label>
                                             </div>
 
                                             <!-- content input-->
-                                            
+
                                             <div class="form-floating mb-3 ">
-                                                <textarea class="form-control" id="content" name="content" rows="30" style="height:100%;"><?= $oldContent ?></textarea>
+                                                <textarea class="form-control" id="content" name="content" rows="30" style="height:100%;"><?=$oldContent?></textarea>
                                                 <label for="content">Contenu</label>
                                             </div>
 
@@ -101,7 +99,7 @@ if (session_id() == '') {
                                         </form>
                                         <br>
                                     </div>
-                                    
+
                                 </div>
                             </div>
                         </div>
