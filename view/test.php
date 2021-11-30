5d48e11a9234d0dfd59ec82f811f19d4f1ca366b
zineb benaicha <zineb.mezlef@gmail.com>
2021-11-30 04:57:10 +0100

    test1-symfony insight

--- view/test.php
+++ view/test.php
@@ -51,53 +51,6 @@ if (session_id() == '') {
                                             $idBlog = $_GET['id_blog'];
                                         }
                                         ?>
-                                        <?php if (isset($blogUpdateSuccess) && !$blogUpdateSuccess):?> 
-                                            <div class="alert alert-danger" role="alert">
-                                                Une erreur est survenue veuillez réessayer plus tard.
-                                            </div>
-                                            <?php header("refresh:2;url=../index.php?action=displayView&viewName=adminDashboard");?>
-
-                                        <? endif ?>
-
-                                        <form action="../index.php?action=updateBlog&id_blog=<?= $idBlog ?>" method="post" >
-                                            <?php if (isset($emptyFields) && $emptyFields): ?> 
-                                                <div class="alert alert-danger" role="alert">
-                                                    Vous devez devez remplir touts les champs !
-                                                </div>
-                                            <? endif ?>
-                                            <!-- Title input-->
-                                            
-                                            <div class="form-floating mb-3 ">
-                                                <input class="form-control" id="title" type="text" name="title" value="<?= $oldTitle ?>" />
-                                                <label for="name">Titre</label>
-                                            </div>
-
-                                            <!-- Chapo input-->
-                                            
-                                            <div class="form-floating mb-3 ">
-                                                <input class="form-control" id="chapo" type="text" name="chapo" value="<?= $oldChapo ?>"/>
-                                                <label for="name">Châpo</label>
-                                            </div>
-
-                                            <!-- author input-->
-                                            
-                                            <div class="form-floating mb-3 ">
-                                                <input class="form-control" id="author" type="text" name="author" value="<?= $oldAuthor ?>"/>
-                                                <label for="name">Auteur</label>
-                                            </div>
-
-                                            <!-- content input-->
-                                            
-                                            <div class="form-floating mb-3 ">
-                                                <textarea class="form-control" id="content" name="content" rows="30" style="height:100%;"><?= $oldContent ?></textarea>
-                                                <label for="content">Contenu</label>
-                                            </div>
-
-                                            <!-- Submition button-->
-                                            <div class="mt-4 mb-0">
-                                                <div class="d-grid"><button class="btn btn-primary btn-block" id="submitButton" type="submit">Appliquer les modifications</button></div>
-                                            </div>
-                                        </form>
                                         <br>
                                     </div>
                                     
