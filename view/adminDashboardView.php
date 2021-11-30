8b9342f4d41fd4d36d373961063dd139ef6f2eb3
zineb benaicha <zineb.mezlef@gmail.com>
2021-11-30 12:32:30 +0100

    customize sidebare in the home dashboard

--- view/adminDashboardView.php
+++ view/adminDashboardView.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_id() == '') {
+    session_start();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,13 +14,14 @@ session_start();
         <title>Espace d'administration-gestion des commentaires</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="../public/css/stylesAdmin.css" rel="stylesheet" />
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
 
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+            <a class="navbar-brand ps-3" href="../index.php?action=displayView&viewName=home">Accueil</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
 
@@ -27,81 +30,48 @@ session_start();
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Settings</a></li>
-                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                        <li><a class="dropdown-item" href="../index.php?action=logout">Se déconnecter</a></li>
                     </ul>
                 </li>
             </ul>
         </nav>
+        
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
+                            <br>
+                            
+                            <a class="nav-link" href="../index.php?action=displayView&viewName=adminDashboard">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt fa-2x"></i></div>
+                                Tableau de bord 
                             </a>
-                            <div class="sb-sidenav-menu-heading">Interface</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Layouts
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            <br>
+                            <a class="nav-link" href="../index.php?action=manageBlogsForAdmin">
+                                <div class="sb-nav-link-icon"><i class="far fa-newspaper fa-2x"></i></div>
+                                Gestion des posts blogs 
                             </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Pages
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            <br>
+                            <a class="nav-link" href="../index.php?action=manageUsersForAdmin">
+                                <div class="sb-nav-link-icon"><i class="fas fa-user fa-2x"></i></div>
+                                Gestion des utilisateurs
                             </a>
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                        Authentication
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="login.html">Login</a>
-                                            <a class="nav-link" href="register.html">Register</a>
-                                            <a class="nav-link" href="password.html">Forgot Password</a>
-                                        </nav>
-                                    </div>
-                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                        Error
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="401.html">401 Page</a>
-                                            <a class="nav-link" href="404.html">404 Page</a>
-                                            <a class="nav-link" href="500.html">500 Page</a>
-                                        </nav>
-                                    </div>
-                                </nav>
-                            </div>
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="charts.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Charts
+                            <br>
+                            <a class="nav-link" href="../index.php?action=displayView&viewName=adminDashboard">
+                                <div class="sb-nav-link-icon"><i class="fas fa-envelope fa-2x"></i></div>
+                                Gestion des messages
                             </a>
-                            <a class="nav-link" href="tables.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
-                            </a>
+                            <br>
+                            <a class="nav-link" href="../index.php?action=manageCommentsForAdmin">
+                                <div class="sb-nav-link-icon"><i class="fas fa-comment-alt fa-2x"></i></div>
+                                Gestion des commentaires
+                            </a>     
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                        <div class="small">Connecté en tant que:</div>
+                        <?= ucfirst($_SESSION['user-name']) ?>
                     </div>
                 </nav>
             </div>
@@ -118,7 +88,7 @@ session_start();
                                 <div class="card bg-primary text-white mb-4">
                                     <div class="card-body">Gestion de blogs posts</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="../index.php?action=manageBlogsForAdmin">View Details</a>
+                                        <a class="small text-white stretched-link" href="../index.php?action=manageBlogsForAdmin">Détailler</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
@@ -127,7 +97,7 @@ session_start();
                                 <div class="card bg-warning text-white mb-4">
                                     <div class="card-body">Gestion d'utilisateurs</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="../index.php?action=manageUsersForAdmin">détailler</a>
+                                        <a class="small text-white stretched-link" href="../index.php?action=manageUsersForAdmin">Détailler</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
@@ -136,7 +106,7 @@ session_start();
                                 <div class="card bg-success text-white mb-4">
                                     <div class="card-body">Gestion de messages</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link" href="#">Détailler</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
@@ -145,7 +115,7 @@ session_start();
                                 <div class="card bg-danger text-white mb-4">
                                     <div class="card-body">Gestion de commentaires</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="../index.php?action=manageCommentsForAdmin">View Details</a>
+                                        <a class="small text-white stretched-link" href="../index.php?action=manageCommentsForAdmin">Détailler</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
