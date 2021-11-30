2474ec1791284f4ba87db859171c8ca305e70847
zineb benaicha <zineb.mezlef@gmail.com>
2021-11-30 14:32:56 +0100

    use templates in admins dashboard views

--- view/blogEditionView.php
+++ view/blogEditionView.php
@@ -3,25 +3,15 @@ if (session_id() == '') {
     session_start();
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Modifier un blog</title>
-        <link href="../public/css/stylesAdmin.css" rel="stylesheet" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="bg-primary">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
+<?php
+$pageTitle = 'Modifier un blog';
+?>
+<?php ob_start();?>
+<div id="layoutAuthentication_content">
                 <main>
                     <div class="container">
                         <div class="row justify-content-center">
-                            <div class="col-lg-12">
+                            <div class="col-lg-11">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header"><h3 class="text-center font-weight-light my-4">Modifier un blog post</h3></div>
                                     <div class="card-body">
@@ -95,7 +85,7 @@ if (session_id() == '') {
 
                                             <!-- Submition button-->
                                             <div class="mt-4 mb-0">
-                                                <div class="d-grid"><button class="btn btn-primary btn-block" id="submitButton" type="submit">Appliquer les modifications</button></div>
+                                                <div class="d-grid"><button class="btn btn-dark btn-block" id="submitButton" type="submit">Appliquer les modifications</button></div>
                                             </div>
                                         </form>
                                         <br>
@@ -106,23 +96,8 @@ if (session_id() == '') {
                         </div>
                     </div>
                 </main>
-            </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="../public/js/scripts.js"></script>
-    </body>
-</html>
+            </div>                   
+<?php $pageContent = ob_get_clean(); ?>
+
+<?php require_once('dashboardTemplateFormular.php'); ?>
+
