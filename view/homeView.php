884624e82901021e5d6baa37f910e5ac203c28b7
zineb benaicha <zineb.mezlef@gmail.com>
2021-12-02 04:51:05 +0100

    add social media icones in footer in home page

--- view/homeView.php
+++ view/homeView.php
@@ -124,7 +124,16 @@ if (isset($_SESSION['user-connected']) && $_SESSION['user-connected']) {
         </section>
         <!-- Footer-->
         <footer class="bg-light py-5">
-            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2021 - Company Name</div></div>
+            <div class="container d-flex justify-content-center">        
+                <!-- Footer Social Icons-->
+                <div>
+                    <h4 class="text-uppercase mb-4">Réseaux sociaux</h4>
+                    <a class="btn btn-outline-dark btn-social mx-1" href="https://www.facebook.com/"><i class="fab fa-fw fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-dark btn-social mx-1" href="https://twitter.com/"><i class="fab fa-fw fa-twitter"></i></a>
+                    <a class="btn btn-outline-dark btn-social mx-1" href="https://www.linkedin.com/"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                    <a class="btn btn-outline-dark btn-social mx-1" href="https://github.com/"><i class="fab fa-fw fa-github"></i></a>
+                </div>
+            </div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
